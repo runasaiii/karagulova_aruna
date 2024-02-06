97ab88966c380dc1bd404426fa113cdbc6929f6c
Aruna <138991935+runasaiii@users.noreply.github.com>
2024-02-06 17:01:34 +0600

    Create ex4.py

---
+++ LAB3/classes/ex4.py
@@ -0,0 +1,33 @@
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(f"Coordinates of the point: ({self.x}, {self.y})")
+
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+
+    def dist(self, other_point):
+        dx = self.x - other_point.x
+        dy = self.y - other_point.y
+        distance = math.sqrt(dx**2 + dy**2)
+        return distance
+
+
+
+point1 = Point(3, 4)
+point2 = Point(6, 8)
+
+point1.show()  
+point2.show() 
+ 
+distance = point1.dist(point2)
+print("Distance between the points:", distance)  
+
+point1.move(10, 12)
+point1.show()
