3a6b8c96fe0b163aacfd1de625aae13bb5a645b6
Aruna <138991935+runasaiii@users.noreply.github.com>
2024-02-10 22:06:02 +0600

    Update ex4.py

--- LAB4/generators/ex4.py
+++ LAB4/generators/ex4.py
@@ -6,5 +6,5 @@ def squares(a,b):
 a,b = map(int,input().split())
 
 numbers = squares(a,b)
-numbers_str = ', '.join(map(str, numbers))
-print(f"Square of all numbers from {a} to {b}: {numbers_str}")
+for elem in numbers:
+    print(elem)
