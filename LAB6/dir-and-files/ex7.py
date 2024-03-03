2c4a64bbe13c0cb0754f44f31e8e6762bd19b32c
Aruna <138991935+runasaiii@users.noreply.github.com>
2024-03-03 15:54:35 +0500

    Create ex7.py

---
+++ LAB6/dir-and-files/ex7.py
@@ -0,0 +1,7 @@
+original_file = open('PP2\\LABS\\LAB6\\p_tasks\\ex7\\original_file.txt', 'r')
+file_contents = original_file.read()
+new_file = open('PP2\\LABS\\LAB6\\p_tasks\\ex7\\new_file.txt', 'w')
+new_file.write(file_contents)
+print('File compied successfully!')
+original_file.close()
+new_file.close()
