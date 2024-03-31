5a2a6b68cb7d733eace5f2047feec0e20d1cf51d
Aruna <138991935+runasaiii@users.noreply.github.com>
2024-03-31 16:45:13 +0500

    Update ex.py

--- LAB8/racer/ex.py
+++ LAB8/racer/ex.py
@@ -1,32 +1,41 @@
+import sys
 import pygame
 from pygame import *
 import random, time
  
+#Инициализация
 pygame.init()
+ 
+#настройка ФПС
 FPS = 60
 FramePerSec = pygame.time.Clock()
  
+#Цвета
 BLUE  = (0, 0, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
+ 
+#Ещё
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 SPEED = 5
 SCORE = 0
-
+ 
+#Фонт
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
  
-background = pygame.image.load("animated_street.png")
-
+background = pygame.image.load("AnimatedStreet.png")
+ 
+#экр
 DISPLAYSURF = pygame.display.set_mode((400,600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+#появление врагов
 class Enemy(pygame.sprite.Sprite):
       def __init__(self):
         super().__init__() 
@@ -41,7 +50,8 @@ class Enemy(pygame.sprite.Sprite):
             SCORE += 1
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
-    
+
+#появление монет           
 class Coin(pygame.sprite.Sprite):
       def __init__(self):
         super().__init__() 
@@ -57,6 +67,8 @@ class Coin(pygame.sprite.Sprite):
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)      
 
 
+
+#функция игрока 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
@@ -78,22 +90,28 @@ class Player(pygame.sprite.Sprite):
               if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(5, 0)
         
-    
+                   
+#Спрайты     
 P1 = Player()
 E1 = Enemy()
 Coin1 = Coin()
+#Creating Sprites Groups
 enemies = pygame.sprite.Group()
 enemies.add(E1)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
 
+# монетки
 coin_add = pygame.sprite.Group()
 
+#новый ивент
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 100000)
-
+ 
+#игровой цикл
 while True:
+    #Cycles through all events occurring  
     for event in pygame.event.get(): 
         if event.type == QUIT:
             pygame.quit()
@@ -102,14 +120,15 @@ while True:
     DISPLAYSURF.blit(background, (0,0))
     scores = font_small.render(str(SCORE), True, BLACK)
     DISPLAYSURF.blit(scores, (10,10))
-  
+ 
+    #движения для спр.
     for entity in all_sprites:
         DISPLAYSURF.blit(entity.image, entity.rect)
         entity.move()
     for entity in coin_add:
         DISPLAYSURF.blit(entity.image, entity.rect)
         entity.move()
-
+    #в случае коллизии между игроком и врагом
     if pygame.sprite.spritecollideany(P1, enemies):
           pygame.mixer.Sound('crash.wav').play()
           time.sleep(0.5)
@@ -124,7 +143,7 @@ while True:
           pygame.quit()
           sys.exit()
     coin_add.add(Coin1)
-
+    #функция получения монет
     if pygame.sprite.spritecollideany(P1, coin_add):
           pygame.mixer.Sound('coin.wav').play()
           for entity in coin_add:
